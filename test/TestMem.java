aad0804a75f11c4afb9140bf1c6e15fcf3a72487
Jesse Willett <jhwjhw@gmail.com>
2010-09-17 16:06:42 +0000

    Extension of test to other classes.  MemCached still not in mix.

--- test/TestMem.java
+++ test/TestMem.java
@@ -18,41 +18,65 @@ public class TestMem
    public static void main ( final String[] argv )
       throws java.io.IOException
    {
-
-      final int limit = 4 * 1024;
+      final MemStats.Stats stats1    = new MemStats.Stats();
+      final MemStats.Stats stats2    = new MemStats.Stats();
+      final int            pageSize  = 1024;
+      final int            pageCount = 4;
+      final int            lineSize  = 64;
+      final int            lineCount = 1;
       final Mem mems[] = {
-         new MemSimple(limit),
-         //new MemStats(),
-         //new MemPaged(),
-         //new MemCached(),
+         new MemSimple(0),
+         new MemSimple(1),
+         new MemSimple(256),
+         // new MemPaged(0,0), // nonpos pageSize
+         // new MemPaged(0,1), // nonpos pageSize
+         new MemPaged(1,0),
+         new MemPaged(1,1),
+         new MemPaged(1,256),
+         new MemPaged(256,1),
+         new MemPaged(16,16),
+         new MemPaged(256,4),
+         new MemPaged(4,256),
+         new MemStats(new MemSimple(pageSize * pageCount),null,null),
+         new MemStats(new MemSimple(pageSize * pageCount),stats1,null),
+         new MemStats(new MemSimple(pageSize * pageCount),null,stats1),
+         new MemStats(new MemSimple(pageSize * pageCount),stats1,stats2),
+         new MemStats(new MemSimple(pageSize * pageCount),stats1,stats1),
+         //new MemCached(new MemSimple(pageSize * pageCount),lineCount,lineSize),
+         //new MemCached(new MemSimple(pageSize * pageCount),lineSize,lineCount),
       };
 
       Random rand = null;
 
-      for ( final Mem mem : mems )
+      for ( int memid = 0; memid < mems.length; ++memid )
       {
-         for ( int addr = 0; addr < limit; ++addr )
+         final Mem mem    = mems[memid];
+         final int length = mem.length();
+         log("memid " + memid + ":");
+         log("  mem:    " + mem);
+         log("  length: " + length);
+         for ( int addr = 0; addr < length; ++addr )
          {
-            final int value1 = limit-addr;
+            final int value1 = length-addr;
             mem.set(addr,value1);
             final int value2 = mem.get(addr);
             assertEquals("addr " + addr,value1,value2);
          }
-         for ( int addr = 0; addr < limit; ++addr )
+         for ( int addr = 0; addr < length; ++addr )
          {
-            final int value1 = limit-addr;
+            final int value1 = length-addr;
             final int value2 = mem.get(addr);
             assertEquals("addr " + addr,value1,value2);
          }
-         for ( int addr = limit-1; addr >= 0; --addr )
+         for ( int addr = length-1; addr >= 0; --addr )
          {
-            final int value1 = limit-addr;
+            final int value1 = length-addr;
             final int value2 = mem.get(addr);
             assertEquals("addr " + addr,value1,value2);
          }
 
          rand = new Random(4321);
-         final int[] addrs = new int[128];
+         final int[] addrs = new int[Math.min(128,length)];
          for ( int i = 0; i < addrs.length; ++i )
          {
             addrs[i] = i;
