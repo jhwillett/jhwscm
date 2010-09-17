5679499ae793c0c38114a9487b402afceca93a5a
Jesse Willett <jhwjhw@gmail.com>
2010-09-17 15:43:30 +0000

    Exploring basic set/get test space.  My random access needs to be more clever: picking random addrs, I end up overwriting then subsequently failing b/c the get half of the test is checking for the old value.  Gonna need a shuffler!

--- test/TestMem.java
+++ test/TestMem.java
@@ -15,23 +15,66 @@ public class TestMem
 {
    private static final boolean verbose = false;
 
-   private static final Random debugRand = new Random(4321);
-
    public static void main ( final String[] argv )
       throws java.io.IOException
    {
 
+      final int limit = 4 * 1024;
       final Mem mems[] = {
-         new MemSimple(1024),
+         new MemSimple(limit),
          //new MemStats(),
          //new MemPaged(),
          //new MemCached(),
       };
 
+      Random rand = null;
+
       for ( final Mem mem : mems )
       {
-         mem.set(10,10);
-         assertEquals(10,mem.get(10));
+         for ( int addr = 0; addr < limit; ++addr )
+         {
+            final int value1 = limit-addr;
+            mem.set(addr,value1);
+            final int value2 = mem.get(addr);
+            assertEquals("addr " + addr,value1,value2);
+         }
+         for ( int addr = 0; addr < limit; ++addr )
+         {
+            final int value1 = limit-addr;
+            final int value2 = mem.get(addr);
+            assertEquals("addr " + addr,value1,value2);
+         }
+         for ( int addr = limit-1; addr >= 0; --addr )
+         {
+            final int value1 = limit-addr;
+            final int value2 = mem.get(addr);
+            assertEquals("addr " + addr,value1,value2);
+         }
+
+         rand = new Random(4321);
+         final int N = 128;
+         for ( int i = 0; i < N; ++i )
+         {
+            final int addr   = rand.nextInt(limit);
+            final int value1 = rand.nextInt();
+            log("set " + addr + " to " + value1);
+            mem.set(addr,value1);
+         }
+         rand = new Random(4321);
+         for ( int i = 0; i < N; ++i )
+         {
+            final int addr   = rand.nextInt(limit);
+            final int value1 = rand.nextInt();
+            final int value2 = mem.get(addr);
+            log("get " + addr + " to " + value2);
+            assertEquals("addr " + addr,value1,value2);
+         }
       }
-   }      
+
+   }
+
+   private static void log ( final Object obj )
+   {
+      System.out.println(obj);
+   }
 }
