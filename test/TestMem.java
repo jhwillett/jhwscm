cd758583c7ff7fab31afa4c3e5d145d197463022
Jesse Willett <jhwjhw@gmail.com>
2010-09-18 14:28:30 +0000

    TestMem reorg.

--- test/TestMem.java
+++ test/TestMem.java
@@ -19,85 +19,77 @@ public class TestMem
    {
       final MemStats.Stats stats1    = new MemStats.Stats();
       final MemStats.Stats stats2    = new MemStats.Stats();
-      final Mem mems[] = {
 
-         new MemSimple(0),
-         new MemSimple(1),
-         new MemSimple(256),
+      test(new MemSimple(0));
+      test(new MemSimple(1));
+      test(new MemSimple(256));
 
-         new MemPaged(1,0),
-         new MemPaged(1,1),
-         new MemPaged(1,256),
-         new MemPaged(256,1),
-         new MemPaged(16,16),
-         new MemPaged(256,4),
-         new MemPaged(4,256),
+      test(new MemPaged(1,0));
+      test(new MemPaged(1,1));
+      test(new MemPaged(1,256));
+      test(new MemPaged(256,1));
+      test(new MemPaged(16,16));
+      test(new MemPaged(256,4));
+      test(new MemPaged(4,256));
 
-         new MemStats(new MemSimple(0),null,null),
-         new MemStats(new MemSimple(0),stats1,null),
-         new MemStats(new MemSimple(0),null,stats1),
-         new MemStats(new MemSimple(0),stats1,stats2),
-         new MemStats(new MemSimple(0),stats1,stats1),
+      test(new MemStats(new MemSimple(0),null,null));
+      test(new MemStats(new MemSimple(0),stats1,null));
+      test(new MemStats(new MemSimple(0),null,stats1));
+      test(new MemStats(new MemSimple(0),stats1,stats2));
+      test(new MemStats(new MemSimple(0),stats1,stats1));
 
-         new MemStats(new MemSimple(1),null,null),
-         new MemStats(new MemSimple(1),stats1,null),
-         new MemStats(new MemSimple(1),null,stats1),
-         new MemStats(new MemSimple(1),stats1,stats2),
-         new MemStats(new MemSimple(1),stats1,stats1),
+      test(new MemStats(new MemSimple(1),null,null));
+      test(new MemStats(new MemSimple(1),stats1,null));
+      test(new MemStats(new MemSimple(1),null,stats1));
+      test(new MemStats(new MemSimple(1),stats1,stats2));
+      test(new MemStats(new MemSimple(1),stats1,stats1));
 
-         new MemStats(new MemSimple(64),null,null),
-         new MemStats(new MemSimple(64),stats1,null),
-         new MemStats(new MemSimple(64),null,stats1),
-         new MemStats(new MemSimple(64),stats1,stats2),
-         new MemStats(new MemSimple(64),stats1,stats1),
+      test(new MemStats(new MemSimple(64),null,null));
+      test(new MemStats(new MemSimple(64),stats1,null));
+      test(new MemStats(new MemSimple(64),null,stats1));
+      test(new MemStats(new MemSimple(64),stats1,stats2));
+      test(new MemStats(new MemSimple(64),stats1,stats1));
 
-         new MemCached(new MemSimple(0),1,1),
-         new MemCached(new MemSimple(0),16,16),
+      test(new MemCached(new MemSimple(0),1,1));
+      test(new MemCached(new MemSimple(0),16,16));
 
-         new MemCached(new MemSimple(1),1,1),
+      test(new MemCached(new MemSimple(1),1,1));
 
-         new MemCached(new MemSimple(256),1,1),
+      test(new MemCached(new MemSimple(256),1,1));
 
-         new MemCached(new MemSimple(256),1,2),
-         new MemCached(new MemSimple(256),2,1),    // fails!
-         new MemCached(new MemSimple(256),1,4),
-         //new MemCached(new MemSimple(256),4,1),    // fails
-         new MemCached(new MemSimple(256),1,8),
-         //new MemCached(new MemSimple(256),8,1),    // fails
-         //new MemCached(new MemSimple(256),8,2),    // fails
+      test(new MemCached(new MemSimple(256),1,2));
+      test(new MemCached(new MemSimple(256),2,1));    // fails!
+      test(new MemCached(new MemSimple(256),1,4));
+      //test(new MemCached(new MemSimple(256),4,1));    // fails
+      test(new MemCached(new MemSimple(256),1,8));
+      //test(new MemCached(new MemSimple(256),8,1));    // fails
+      //test(new MemCached(new MemSimple(256),8,2));    // fails
 
-         new MemCached(new MemSimple(256),16,16),
+      test(new MemCached(new MemSimple(256),16,16));
 
-         new MemCached(new MemSimple(1024),1,1),
-         //new MemCached(new MemSimple(1024),2,1),   // fails!
+      test(new MemCached(new MemSimple(1024),1,1));
+      //test(new MemCached(new MemSimple(1024),2,1));   // fails!
 
-         //new MemCached(new MemSimple(256),2,16),   // fails!
-         //new MemCached(new MemSimple(256),16,2),   // fails!
+      //test(new MemCached(new MemSimple(256),2,16));   // fails!
+      //test(new MemCached(new MemSimple(256),16,2));   // fails!
 
-         //new MemCached(new MemSimple(256),512,16), // fails!
+      //test(new MemCached(new MemSimple(256),512,16)); // fails!
 
-         new MemCached(new MemSimple(256),16,512),
+      test(new MemCached(new MemSimple(256),16,512));
 
-         //new MemCached(new MemSimple(256),2,2),    // fails!
-         //new MemCached(new MemSimple(256),4,4),    // fails!
-         //new MemCached(new MemSimple(256),8,8),    // fails!
-         new MemCached(new MemSimple(256),16,16),
-      };
-
-      for ( int memid = 0; memid < mems.length; ++memid )
-      {
-         final Mem mem    = mems[memid];
-         test(mem,memid);
-      }
+      //test(new MemCached(new MemSimple(256),2,2));    // fails!
+      //test(new MemCached(new MemSimple(256),4,4));    // fails!
+      //test(new MemCached(new MemSimple(256),8,8));    // fails!
+      test(new MemCached(new MemSimple(256),16,16));
    }
 
-   private static void test ( final Mem mem, final int memid )
+   private static void test ( final Mem mem )
    {
       Random rand = null;
 
       final int length = mem.length();
-      log("memid " + memid + ":");
-      log("  mem:    " + mem);
+      //log("memid " + memid + ":");
+      log("mem:    " + mem);
       log("  length: " + length);
       for ( int addr = 0; addr < length; ++addr )
       {
