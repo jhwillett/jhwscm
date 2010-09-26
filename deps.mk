07021e7b49f9ec7a9bc9e3881d5964c7456a92bb
Jesse Willett <jhwjhw@gmail.com>
2010-09-26 18:33:18 +0000

    'make getdeps' reorg

--- deps.mk
+++ deps.mk
@@ -1,11 +1,5 @@
 
-FILES += junit-4.8.2.jar
-FILES += cobertura-1.9.4.1-bin.tar.bz2
-FILES += pmd-bin-4.2.5.zip
-FILES += jdepend-2.9.zip
-FILES += javancss-32.53.zip
-FILES += checkstyle-5.2-bin.tar.gz
-
+FILES := $(shell ls -1 deps/*.url | sed 's:^deps/\(.*\).url$$:\1:g' )
 JARS  := $(filter %.jar,$(FILES))
 TBZS  := $(filter %.tar.bz2,$(FILES))
 TGZS  := $(filter %.tar.gz,$(FILES))
@@ -18,14 +12,14 @@ $(FILES:%=getdep-%): getdep-%: $(DESTDIR)/%.crack.ok
 # download the files
 $(FILES:%=$(DESTDIR)/%): $(DESTDIR)/%: 
 	mkdir -p $(dir $@)
-	curl `cat $*.url` --location --output $@.tmp
+	curl `cat deps/$*.url` --location --output $@.tmp
 	mv $@.tmp $@
 
 # confirm file signatures
 $(FILES:%=$(DESTDIR)/%.md5.ok): $(DESTDIR)/%.md5.ok: $(DESTDIR)/%
 	mkdir -p $(dir $@)
 	md5sum $< | cut -f 1 -d ' ' > $@.md5.actual
-	diff -wB $(notdir $<).md5 $@.md5.actual
+	diff -wB deps/$(notdir $<).md5 $@.md5.actual
 	touch $@
 
 # open the files, varies by file type
