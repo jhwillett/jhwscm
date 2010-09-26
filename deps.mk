9b365037c899dd465d73484c77531350df1dc1c9
Jesse Willett <jhwjhw@gmail.com>
2010-09-26 18:10:27 +0000

    wrestling w/ 'make getdeps'

--- deps.mk
+++ deps.mk
@@ -1,10 +1,10 @@
-LIBS            += junit
-LIBS            += cobertura
-LIBS            += pmd
-LIBS            += pmd2
-LIBS            += jdepend
-LIBS            += javancss
-LIBS            += checkstyle
+LIBS  += junit
+LIBS  += cobertura
+LIBS  += pmd
+LIBS  += pmd2
+LIBS  += jdepend
+LIBS  += javancss
+LIBS  += checkstyle
 
 FILES += junit-4.8.2.jar
 FILES += cobertura-1.9.4.1-bin.tar.bz2
@@ -18,7 +18,7 @@ FILES += checkstyle-5.2-bin.tar.gz
 getdeps: $(LIBS:%=getdep-%)
 $(LIBS:%=getdep-%): getdep-%: $(DESTDIR)/getdep-%.ok
 
-$(DESTDIR)/getdep-junit.ok:      $(DESTDIR)/junit-4.8.2.jar
+$(DESTDIR)/getdep-junit.ok:      $(DESTDIR)/$(shell cat junit.file)
 $(DESTDIR)/getdep-cobertura.ok:  $(DESTDIR)/cobertura-1.9.4.1-bin.tar.bz2
 $(DESTDIR)/getdep-pmd.ok:        $(DESTDIR)/pmd-src-4.2.5.zip
 $(DESTDIR)/getdep-pmd2.ok:       $(DESTDIR)/pmd-bin-4.2.5.zip
@@ -27,14 +27,20 @@ $(DESTDIR)/getdep-javancss.ok:   $(DESTDIR)/javancss-32.53.zip
 $(DESTDIR)/getdep-checkstyle.ok: $(DESTDIR)/checkstyle-5.2-bin.tar.gz
 
 $(LIBS:%=$(DESTDIR)/getdep-%.ok): $(DESTDIR)/getdep-%.ok:
-	@echo "pattern $*"
-	@echo "output  $@"
-	@echo "input   $<"
+	mkdir -p $(dir $@)
 	md5sum $< | cut -f 1 -d ' ' > $@.md5.actual
-	diff $(notdir $<).md5 $@.md5.actual
+	diff -w $(notdir $<).md5 $@.md5.actual
 	touch $@
 
 $(FILES:%=$(DESTDIR)/%): $(DESTDIR)/%: 
 	mkdir -p $(dir $@)
 	curl `cat $*.url` --location --output $@.tmp
 	mv $@.tmp $@
+
+.PHONY: crack $(FILES:%=crack-%)
+crack: $(FILES:%=crack-%)
+$(FILES:%=crack-%): crack-%: $(DESTDIR)/crack-%.ok
+$(FILES:%=$(DESTDIR)/crack-%.ok): $(DESTDIR)/crack-%.ok: $(DESTDIR)/%
+	@echo "pattern $*"
+	@echo "target  $@"
+	false
