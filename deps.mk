9e1b855a593807ce5ab7ecadd276dcdd92cdb065
Jesse Willett <jhwjhw@gmail.com>
2010-09-26 18:26:30 +0000

    Getting there with 'make deps'.

--- deps.mk
+++ deps.mk
@@ -1,46 +1,42 @@
-LIBS  += junit
-LIBS  += cobertura
-LIBS  += pmd
-LIBS  += pmd2
-LIBS  += jdepend
-LIBS  += javancss
-LIBS  += checkstyle
 
 FILES += junit-4.8.2.jar
 FILES += cobertura-1.9.4.1-bin.tar.bz2
-FILES += pmd-src-4.2.5.zip
 FILES += pmd-bin-4.2.5.zip
 FILES += jdepend-2.9.zip
 FILES += javancss-32.53.zip
 FILES += checkstyle-5.2-bin.tar.gz
 
-.PHONY: getdeps $(LIBS:%=getdep-%)
-getdeps: $(LIBS:%=getdep-%)
-$(LIBS:%=getdep-%): getdep-%: $(DESTDIR)/getdep-%.ok
+JARS  := $(filter %.jar,$(FILES))
+TBZS  := $(filter %.tar.bz2,$(FILES))
+TGZS  := $(filter %.tar.gz,$(FILES))
+ZIPS  := $(filter %.zip,$(FILES))
 
-$(DESTDIR)/getdep-junit.ok:      $(DESTDIR)/$(shell cat junit.file)
-$(DESTDIR)/getdep-cobertura.ok:  $(DESTDIR)/cobertura-1.9.4.1-bin.tar.bz2
-$(DESTDIR)/getdep-pmd.ok:        $(DESTDIR)/pmd-src-4.2.5.zip
-$(DESTDIR)/getdep-pmd2.ok:       $(DESTDIR)/pmd-bin-4.2.5.zip
-$(DESTDIR)/getdep-jdepend.ok:    $(DESTDIR)/jdepend-2.9.zip
-$(DESTDIR)/getdep-javancss.ok:   $(DESTDIR)/javancss-32.53.zip
-$(DESTDIR)/getdep-checkstyle.ok: $(DESTDIR)/checkstyle-5.2-bin.tar.gz
+.PHONY: getdeps $(FILES:%=getdep-%)
+getdeps: $(FILES:%=getdep-%)
+$(FILES:%=getdep-%): getdep-%: $(DESTDIR)/%.crack.ok
 
-$(LIBS:%=$(DESTDIR)/getdep-%.ok): $(DESTDIR)/getdep-%.ok:
-	mkdir -p $(dir $@)
-	md5sum $< | cut -f 1 -d ' ' > $@.md5.actual
-	diff -w $(notdir $<).md5 $@.md5.actual
-	touch $@
-
+# download the files
 $(FILES:%=$(DESTDIR)/%): $(DESTDIR)/%: 
 	mkdir -p $(dir $@)
 	curl `cat $*.url` --location --output $@.tmp
 	mv $@.tmp $@
 
-.PHONY: crack $(FILES:%=crack-%)
-crack: $(FILES:%=crack-%)
-$(FILES:%=crack-%): crack-%: $(DESTDIR)/crack-%.ok
-$(FILES:%=$(DESTDIR)/crack-%.ok): $(DESTDIR)/crack-%.ok: $(DESTDIR)/%
-	@echo "pattern $*"
-	@echo "target  $@"
-	false
+# confirm file signatures
+$(FILES:%=$(DESTDIR)/%.md5.ok): $(DESTDIR)/%.md5.ok: $(DESTDIR)/%
+	mkdir -p $(dir $@)
+	md5sum $< | cut -f 1 -d ' ' > $@.md5.actual
+	diff -wB $(notdir $<).md5 $@.md5.actual
+	touch $@
+
+# open the files, varies by file type
+$(JARS:%=$(DESTDIR)/%.crack.ok): $(DESTDIR)/%.crack.ok: $(DESTDIR)/%.md5.ok
+	touch $@
+$(TBZS:%=$(DESTDIR)/%.crack.ok): $(DESTDIR)/%.crack.ok: $(DESTDIR)/%.md5.ok
+	cd $(DESTDIR) && tar xvfj $*
+	touch $@
+$(TGZS:%=$(DESTDIR)/%.crack.ok): $(DESTDIR)/%.crack.ok: $(DESTDIR)/%.md5.ok
+	cd $(DESTDIR) && tar xvfz $*
+	touch $@
+$(ZIPS:%=$(DESTDIR)/%.crack.ok): $(DESTDIR)/%.crack.ok: $(DESTDIR)/%.md5.ok
+	cd $(DESTDIR) && unzip $*
+	touch $@
