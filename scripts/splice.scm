e74a3f500e4715f2e0e26d785b4d6b1e4f297530
Jesse Willett <jhwjhw@gmail.com>
2010-10-14 15:43:46 +0000

    Bit more dev in scripts/splice.scm

--- scripts/splice.scm
+++ scripts/splice.scm
@@ -15,17 +15,35 @@
   (display (func expr))
   (newline))
 
+(define bad-splice 'failure-bad-splice)
+
 (define (splice expr)
   (if (not (pair? expr))
       expr
       (let ((head (car expr))
             (rest (cdr expr)))
-        (if (not (pair? head))
-            (cons (splice head) (splice rest))
-            (let ((headfirst (car head)))
-              (if (not (eq? 'spliceme headfirst))
-                  (cons (splice head) (splice rest))
-                  (append (car (cdr head)) (splice rest))))))))
+        (if (eq? 'spliceme head)
+            bad-splice
+            (if (not (pair? head))
+                (cons (splice head) (splice rest))
+                (let ((headfirst (car head)))
+                  (if (not (eq? 'spliceme headfirst))
+                      (cons (splice head) (splice rest))
+                      (append (car (cdr head)) (splice rest)))))))))
+
+
+
+(exhibit splice '(1 (spliceme (2 3)))      '(1 2 3)      )
+(exhibit splice '(1 (spliceme (2 3)) 4)    '(1 2 3 4)    )
+(exhibit splice '(1 ((spliceme (2 3))) 4)  '(1 (2 3) 4)  )
+(exhibit splice '(1 4 ((spliceme (2 3))))  '(1 4 (2 3))  )
+(exhibit splice '(1 4 ((spliceme ())))     '(1 4 ())     )
+(exhibit splice '(1 4 (spliceme ()))       '(1 4)        )
+
+(exhibit splice '(spliceme 2)              bad-splice    )
+(exhibit splice '((spliceme 2))            '2            )
+(exhibit splice '(1 (spliceme 2))          '(1 . 2)      )
+(exhibit splice '(1 (spliceme 2) 3)        bad-splice    )
 
 ;;            { "`(1 ,@(list 2 3))",             "(1 2 3)"     },
 ;;            { "`(1 ,(list 2 3))",              "(1 (2 3))"   },
@@ -36,10 +54,3 @@
 ;;            { "`(1 ,@2 3)",                    SEMANTIC      },
 ;;            { "` (1 ,@ 2 3)",                  SEMANTIC      },
 ;;            { "` (1 ,@2 3)",                   SEMANTIC      },
-
-
-(exhibit splice '(1 (spliceme (2 3)))    '(1 2 3)    )
-(exhibit splice '(1 (spliceme (2 3)) 4)  '(1 2 3 4)  )
-(exhibit splice '((spliceme 2))          '2          )
-(exhibit splice '(1 (spliceme 2))        '(1 . 2)    )
-(exhibit splice '(1 (spliceme 2) 3)      '(whatever) ) ;; err expected
